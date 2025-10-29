885e7723407aea9ba42a6eb28fa7838d94a4518c
SandyaSuresh <sandya.suresh@gmail.com>
2025-10-29 13:48:10 -0400

    wiggling_robot

--- generate.py
+++ generate.py
@@ -1,49 +1,55 @@
 import pyrosim.pyrosim as pyrosim
 
+def World(n,xnum,ynum):
+    pyrosim.Start_SDF("boxes.sdf")
+    for x in range(xnum):
+        for y in range(ynum):
+            for i in range(n):
+                s = 1 - i/n
+                pyrosim.Send_Cube(name="Box", pos=[x,y,0.5 + i] , size=[s,s,s])
+    pyrosim.End()
 
 def Robot():
+    pyrosim.Start_URDF("robot.urdf")
+
     h = 0.3
     w = 0.1
-    pyrosim.Start_URDF("robot.urdf")
 
     # Main Body
     pyrosim.Send_Cube(name="0", pos=[0,0,(h + h/6)], size=[h, h, h])
 
-    # Front Leg
-    pyrosim.Send_Joint(name="0-1",type="fixed", position=[0,0,0], parent="0", child="1", axis=[0,1,0], lower=-3, upper=3)
-    pyrosim.Send_Cube(name="1", pos=[0, h/2 + (w/2), (2*h)/3], size=[w, w, h/2])
-    pyrosim.Send_Joint(name="1-2", type="revolute", position=[0,w/2, 0], parent="1", child="2", axis=[0,1,0], lower=-3, upper=3)
+    # Right Leg
+    pyrosim.Send_Joint(name="0_1",type="revolute", position=[0,0,0], parent="0", child="1")
+    pyrosim.Send_Cube(name="1", pos=[0, h/2 + (w/2), (2*h)/3], size=[w, w, h/2])   
+    pyrosim.Send_Joint(name="1_2", type="revolute", position=[0,w/2, 0], parent="1", child="2")
     pyrosim.Send_Cube(name="2", pos=[0, h/2, h/6], size=[w, w, h/2])
 
 
-    # Front Leg
-    pyrosim.Send_Joint(name="0-3",type="fixed", position=[0,0,0], parent="0", child="3", axis=[1,0,0], lower=-3, upper=3)
+    # Left Leg
+    pyrosim.Send_Joint(name="0_3",type="revolute", position=[0,0,0], parent="0", child="3")
     pyrosim.Send_Cube(name="3", pos=[0, -h/2 - (w/2), (2*h)/3], size=[w, w, h/2])
-    pyrosim.Send_Joint(name="3-4", type="revolute", position=[0,-w/2, 0], parent="3", child="4", axis=[1,0,0], lower=-3, upper=3)
+    pyrosim.Send_Joint(name="3_4", type="revolute", position=[0,-w/2, 0], parent="3", child="4")
     pyrosim.Send_Cube(name="4", pos=[0, -h/2, h/6], size=[w, w, h/2])
 
     # Front Leg
-    pyrosim.Send_Joint(name="0-5",type="fixed", position=[0,0,0], parent="0", child="5", axis=[0,1,0], lower=-3, upper=3)
+    pyrosim.Send_Joint(name="0_5",type="fixed", position=[0,0,0], parent="0", child="5")
     pyrosim.Send_Cube(name="5", pos=[-h/2 - (w/2), 0, (2*h)/3], size=[w, w, h/2])
-    pyrosim.Send_Joint(name="5-6", type="revolute", position=[-w/2,0, 0], parent="5", child="6", axis=[0,1,0], lower=-3, upper=3)
+    pyrosim.Send_Joint(name="5_6", type="revolute", position=[-w/2,0, 0], parent="5", child="6")
     pyrosim.Send_Cube(name="6", pos=[-h/2, 0, h/6], size=[w, w, h/2])
 
     # Front Leg
-    pyrosim.Send_Joint(name="0-7",type="fixed", position=[0,0,0], parent="0", child="7", axis=[1,0,0], lower=-3, upper=3)
+    pyrosim.Send_Joint(name="0_7",type="fixed", position=[0,0,0], parent="0", child="7")
     pyrosim.Send_Cube(name="7", pos=[h/2 + (w/2), 0, (2*h)/3], size=[w, w, h/2])
-    pyrosim.Send_Joint(name="7-8", type="revolute", position=[w/2,0, 0], parent="7", child="8", axis=[1,0,0], lower=-3, upper=3)
+    pyrosim.Send_Joint(name="7_8", type="revolute", position=[w/2,0, 0], parent="7", child="8")
     pyrosim.Send_Cube(name="8", pos=[h/2, 0, h/6], size=[w, w, h/2])
-  
-    pyrosim.End()
 
-def World(n, xnum, ynum):
-    pyrosim.Start_SDF("box.sdf")
-    for x in range(xnum):
-        for y in range(ynum):
-            for i in range(n):
-                s = 1-1/(i+1)
-                pyrosim.Send_Cube(name="Box1", pos=[x,y,0.5 + i] , size=[s, s, s])
-
+    # pyrosim.Send_Cube(name="0", pos=[0,0,0.5] , size=[1,1,1]) ## Cannot 
+    # pyrosim.Send_Joint(name="0_1", parent="0", child="1", type = "revolute", position=[0.5,0,1])
+    # pyrosim.Send_Cube(name="1", pos=[0.5,0,0.5] , size=[1,1,1])
+    # pyrosim.Send_Joint(name="1_2", parent="1", child="2", type = "revolute", position=[1,0,0])
+    # pyrosim.Send_Cube(name="2", pos=[0.5,0,-0.5] , size=[1,1,1])
     pyrosim.End()
 
 Robot()
+
+#World(10,5,5)
--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -77,6 +77,7 @@ def Prepare_Link_Dictionary(bodyID):
         jointName = jointName.split("_")
 
         linkName = jointName[1]
+        print("Prepare Links: ",linkName)
 
         linkNamesToIndices[linkName] = jointIndex
 
@@ -96,7 +97,8 @@ def Prepare_Joint_Dictionary(bodyID):
 
         jointInfo = p.getJointInfo( bodyID , jointIndex )
 
-        jointName = jointInfo[1]
+        jointName = jointInfo[1].decode("utf-8")
+        print("Prepare Joints ",jointName)
 
         jointNamesToIndices[jointName] = jointIndex
 
@@ -134,9 +136,9 @@ def Send_Cube(name="default",pos=[0,0,0],size=[1,1,1]):
 
     availableLinkIndex = availableLinkIndex + 1
 
-def Send_Joint(name,parent,child,type,position,axis,lower,upper):
+def Send_Joint(name,parent,child,type,position):
 
-    joint = JOINT(name,parent,child,type,position,axis,lower,upper)
+    joint = JOINT(name,parent,child,type,position)
 
     joint.Save(f)
 
